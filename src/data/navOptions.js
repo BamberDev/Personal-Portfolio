60d744fae8d7af7b0791ab868aa8e477c08b7b55
Bamberek <kabaczek99@gmail.com>
2024-03-17 23:33:06 +0100

    updated navbar options

--- src/data/navOptions.js
+++ src/data/navOptions.js
@@ -1,6 +1,6 @@
 export const navOptions = [
   { name: "HOME", path: "/" },
-  { name: "ABOUT", path: "/experience" },
+  { name: "ABOUT", path: "#about", isScroll: true },
   { name: "PROJECTS", path: "/projects" },
   { name: "CONTACT", path: "#contact", isScroll: true },
 ];
