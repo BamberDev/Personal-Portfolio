91af23b5e5d7adfc79b047374575ca291edb3aba
Bamberek <kabaczek99@gmail.com>
2024-03-20 19:22:56 +0100

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <div className="container">
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </div>
       <Footer />
     </Router>
