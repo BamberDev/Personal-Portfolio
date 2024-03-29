d4526440d3ff5f3c87b6abb002ced7ad19cb9d70
Bamberek <kabaczek99@gmail.com>
2024-03-29 03:40:44 +0100

    updated landing page

--- src/pages/HomePage/components/LandingPage/LandingPage.jsx
+++ src/pages/HomePage/components/LandingPage/LandingPage.jsx
@@ -5,23 +5,26 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 const LandingPage = () => {
   return (
     <section className={styles.videoContainer}>
-      <video autoPlay loop muted className={styles.video}>
+      <video
+        autoPlay
+        loop
+        muted
+        alt="Background video showing grey triangles"
+        className={styles.video}
+      >
         <source src={videoSource} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className={clsx("container", styles.landingPageContainer)}>
         <h1>
-          HI THERE,
+          <span className={styles.topHeading}>HI THERE,</span>
           <span className={styles.bottomHeading}>I&apos;M KEVIN.</span>
         </h1>
         <h2>FRONTEND DEVELOPER</h2>
         <p>
-          <span className={styles.topParagraph}>
-            I build pixel-perfect, user-friendly
-          </span>
+          <span>I build pixel-perfect, user-friendly</span>
           and immersive digital experiences.
         </p>
-
         <MdKeyboardArrowDown />
       </div>
     </section>
