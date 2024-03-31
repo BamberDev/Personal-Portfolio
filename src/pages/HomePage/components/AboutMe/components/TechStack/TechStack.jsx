b1f2c3fa231b2c86dce0462f58f446ac8577f94b
Bamberek <kabaczek99@gmail.com>
2024-03-31 23:37:41 +0200

    added tech stack

---
+++ src/pages/HomePage/components/AboutMe/components/TechStack/TechStack.jsx
@@ -0,0 +1,23 @@
+import { nanoid } from "nanoid";
+import styles from "./TechStack.module.scss";
+import { techStackOptions } from "../../../../../../data/techStackOptions";
+
+const TechStack = () => {
+  return (
+    <div className={styles.toolSection}>
+      <h1>TOOLS I USE</h1>
+      <div className={styles.toolList}>
+        {techStackOptions.map((tool) => (
+          <div className={styles.toolCard} key={nanoid()}>
+            <a href={tool.url} target="_blank" rel="noopener noreferrer">
+              <img src={tool.icon} alt={tool.alt} />
+            </a>
+            <p>{tool.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TechStack;
