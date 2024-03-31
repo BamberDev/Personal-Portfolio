2ed3cb4d8c3ffbae7e23bdc075ba1405385b79ac
Bamberek <kabaczek99@gmail.com>
2024-03-31 23:44:40 +0200

    added tech stack options

---
+++ src/data/techStackOptions.js
@@ -0,0 +1,52 @@
+import JavaScriptIcon from "./../assets/TechStackIcons/javascript.svg";
+import ReactIcon from "./../assets/TechStackIcons/react.svg";
+import CssIcon from "./../assets/TechStackIcons/css.svg";
+import HtmlIcon from "./../assets/TechStackIcons/html.svg";
+import BootstrapIcon from "./../assets/TechStackIcons/bootstrap.svg";
+import GitIcon from "./../assets/TechStackIcons/git.svg";
+import SassIcon from "./../assets/TechStackIcons/sass.svg";
+
+export const techStackOptions = [
+  {
+    alt: "Html icon",
+    icon: HtmlIcon,
+    description: "HTML",
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    alt: "Css icon",
+    icon: CssIcon,
+    description: "CSS",
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    alt: "JavaScript icon",
+    icon: JavaScriptIcon,
+    description: "JAVASCRIPT",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    alt: "React icon",
+    icon: ReactIcon,
+    description: "REACT",
+    url: "https://react.dev/",
+  },
+  {
+    alt: "Bootstrap icon",
+    icon: BootstrapIcon,
+    description: "BOOTSTRAP",
+    url: "https://getbootstrap.com/",
+  },
+  {
+    alt: "Sass icon",
+    icon: SassIcon,
+    description: "SASS/SCSS",
+    url: "https://sass-lang.com/",
+  },
+  {
+    alt: "Git icon",
+    icon: GitIcon,
+    description: "GIT",
+    url: "https://git-scm.com/",
+  },
+];
