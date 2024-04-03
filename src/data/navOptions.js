887799fe4e849872a428dd53bae3d5149496e118
Bamberek <kabaczek99@gmail.com>
2024-04-03 22:21:47 +0200

    renamed navbar options

--- src/data/navOptions.js
+++
@@ -1,6 +0,0 @@
-export const navOptions = [
-  { name: "HOME", path: "/" },
-  { name: "ABOUT", path: "/#about", isScroll: true },
-  { name: "PROJECTS", path: "/#projects", isScroll: true },
-  { name: "CONTACT", path: "/contact" },
-];
---
+++ src/data/navbarOptions.js
@@ -0,0 +1,24 @@
+export const navbarOptions = [
+  {
+    name: "HOME",
+    path: "/",
+    showOnContact: true,
+  },
+  {
+    name: "ABOUT",
+    path: "/#about",
+    isScroll: true,
+    showOnContact: false,
+  },
+  {
+    name: "PROJECTS",
+    path: "/#projects",
+    isScroll: true,
+    showOnContact: false,
+  },
+  {
+    name: "CONTACT",
+    path: "/contact",
+    showOnContact: false,
+  },
+];
