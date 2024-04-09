57050e89135ca170aabe2a186a27e7d8d158c06b
Bamberek <kabaczek99@gmail.com>
2024-04-09 02:56:11 +0200

    added want to learn options

---
+++ src/data/wantToLearnOptions.js
@@ -0,0 +1,45 @@
+import NextIcon from "./../assets/WantToLearnIcons/nextjs.svg";
+import TypeScriptIcon from "./../assets/WantToLearnIcons/typescript.svg";
+import TailwindIcon from "./../assets/WantToLearnIcons/tailwind.svg";
+import GSAPIcon from "./../assets/WantToLearnIcons/gsap.svg";
+import NodeIcon from "./../assets/WantToLearnIcons/nodejs.svg";
+import MongoDBIcon from "./../assets/WantToLearnIcons/mongodb.svg";
+
+export const wantToLearnOptions = [
+  {
+    alt: "TypeScript icon",
+    icon: TypeScriptIcon,
+    description: "TYPESCRIPT",
+    url: "https://www.typescriptlang.org/",
+  },
+  {
+    alt: "NextJS icon",
+    icon: NextIcon,
+    description: "NEXT.JS",
+    url: "https://nextjs.org/",
+  },
+  {
+    alt: "Tailwind icon",
+    icon: TailwindIcon,
+    description: "TAILWIND",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    alt: "GSAP icon",
+    icon: GSAPIcon,
+    description: "GSAP",
+    url: "https://gsap.com/",
+  },
+  {
+    alt: "NodeJS icon",
+    icon: NodeIcon,
+    description: "NODE.JS",
+    url: "https://nodejs.org/en",
+  },
+  {
+    alt: "MongoDB icon",
+    icon: MongoDBIcon,
+    description: "MONGODB",
+    url: "https://www.mongodb.com/",
+  },
+];
