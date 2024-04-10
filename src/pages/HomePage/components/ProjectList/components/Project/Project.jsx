5ebf6567becc78b69f2e34a0c794e6786bbd31ce
Bamberek <kabaczek99@gmail.com>
2024-04-11 00:01:45 +0200

    added project component

---
+++ src/pages/HomePage/components/ProjectList/components/Project/Project.jsx
@@ -0,0 +1,53 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styles from "./Project.module.scss";
+import { useEffect, useState } from "react";
+
+const Project = ({ index, title, description, path, alt, src, setModal }) => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div
+      onMouseEnter={() => {
+        setModal({ active: true, index });
+      }}
+      onMouseLeave={() => {
+        setModal({ active: false, index });
+      }}
+      className={styles.project}
+    >
+      <Link to={path}>
+        {viewportWidth < 1024 && (
+          <div className={styles.imageContainer}>
+            <img src={src} alt={alt} />
+          </div>
+        )}
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </Link>
+    </div>
+  );
+};
+
+Project.propTypes = {
+  index: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  setModal: PropTypes.func.isRequired,
+};
+export default Project;
