2bd7b4258fe8c8014089461cb8eb49df965f43f4
Bamberek <kabaczek99@gmail.com>
2024-04-10 23:59:05 +0200

    added project list options

---
+++ src/data/projectListOptions.js
@@ -0,0 +1,50 @@
+import image1 from "./../assets/ProjectImages/SpaceInvadersImage.svg";
+import image2 from "./../assets/ProjectImages/PersonalWebsiteImage.svg";
+import image3 from "./../assets/ProjectImages/ExpenseTrackerImage.svg";
+import image4 from "./../assets/ProjectImages/UrlShortenerImage.svg";
+import image5 from "./../assets/ProjectImages/CurrencyConverterImage.svg";
+
+const projects = [
+  {
+    title: "SPACE INVADERS",
+    src: image1,
+    color: "#3a3a3a",
+    description: "HTML CSS JS",
+    path: "",
+    alt: "Image of my space invaders project",
+  },
+  {
+    title: "PERSONAL WEBSITE",
+    src: image2,
+    color: "#282828",
+    description: "REACT  CSS/SCSS ",
+    path: "",
+    alt: "Image of my personal website landing page",
+  },
+  {
+    title: "EXPENSE TRACKER",
+    src: image3,
+    color: "#3a3a3a",
+    description: "HTML CSS/SCSS JS",
+    path: "",
+    alt: "Image of my expense tracker project",
+  },
+  {
+    title: "URL SHORTENER",
+    src: image4,
+    color: "#282828",
+    description: "HTML CSS/SCSS JS",
+    path: "",
+    alt: "Image of my url shortener project",
+  },
+  {
+    title: "CURRENCY CONVERTER",
+    src: image5,
+    color: "#3a3a3a",
+    description: "HTML CSS/SCSS JS",
+    path: "",
+    alt: "Image of my currency converter project",
+  },
+];
+
+export default projects;
