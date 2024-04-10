26ef3519fef7e027934f1a302940a146629f18d8
Bamberek <kabaczek99@gmail.com>
2024-04-10 23:57:16 +0200

    updated home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import styles from "./HomePage.module.scss";
 import AboutMe from "./components/AboutMe/AboutMe";
 import LandingPage from "./components/LandingPage/LandingPage";
+import ProjectList from "./components/ProjectList/ProjectList";
 
 const HomePage = () => {
   return (
     <div className={styles.homePageContainer}>
       <LandingPage />
       <AboutMe />
+      <ProjectList />
     </div>
   );
 };
