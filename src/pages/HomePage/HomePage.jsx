bde39a5395cb8b8708cdaf8977616f9f011a20c2
Bamberek <kabaczek99@gmail.com>
2024-04-13 04:36:24 +0200

    added preloader to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,37 @@
+import { useEffect } from "react";
+import PropTypes from "prop-types";
 import AboutMe from "./components/AboutMe/AboutMe";
 import LandingPage from "./components/LandingPage/LandingPage";
 import ProjectList from "./components/ProjectList/ProjectList";
+import Preloader from "./components/Preloader/Preloader";
+import { AnimatePresence } from "framer-motion";
 
-const HomePage = () => {
+const HomePage = ({ isLoading, setIsLoading }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      document.body.style.cursor = "default";
+      window.scrollTo(0, 0);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [setIsLoading]);
+
   return (
     <div>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <LandingPage />
       <AboutMe />
       <ProjectList />
     </div>
   );
+};
+
+HomePage.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  setIsLoading: PropTypes.func.isRequired,
 };
 
 export default HomePage;
