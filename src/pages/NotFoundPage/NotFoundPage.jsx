ff0dac716e27b5d56ef7768652984fce8cfa1023
Bamberek <kabaczek99@gmail.com>
2024-04-20 01:30:28 +0200

    added aria label

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -8,7 +8,11 @@ const NotFoundPage = () => {
     <div className={clsx("container", styles.notFoundPageContainer)}>
       <h1>SEEMS LIKE YOU GOT LOST :(</h1>
       <h2>404 | Page not found.</h2>
-      <Link to="/" className={styles.homeButton}>
+      <Link
+        to="/"
+        className={styles.homeButton}
+        aria-label="Go back home button"
+      >
         <span>GO BACK HOME</span>
         <i></i>
         <FaArrowRotateLeft />
