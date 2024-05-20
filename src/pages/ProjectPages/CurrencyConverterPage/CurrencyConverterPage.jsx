fa7828e4a0f81d9ef5315f4f59c8671988984cc1
Bamberek <kabaczek99@gmail.com>
2024-05-20 23:53:57 +0200

    updated project description

--- src/pages/ProjectPages/CurrencyConverterPage/CurrencyConverterPage.jsx
+++ src/pages/ProjectPages/CurrencyConverterPage/CurrencyConverterPage.jsx
@@ -6,15 +6,8 @@ const CurrencyConverterPage = () => {
 
   const content = (
     <p>
-      This Currency Converter application is built with React, SCSS, and
-      Bootstrap, utilizing the free API provided by the National Bank of Poland
-      (NBP) to fetch the latest exchange rates for Euro (EUR), US Dollar (USD),
-      and Swiss Franc (CHF). The app allows users to convert the selected
-      currency to Polish Zloty (PLN). Features Real-Time Exchange Rates: The
-      application fetches the latest exchange rates from the NBP API, ensuring
-      accuracy and up-to-date currency conversions. User-Friendly Interface: The
-      app provides a simple and intuitive user interface for easy navigation and
-      efficient currency conversion.
+      This application allows you to convert amounts between different
+      currencies using the latest exchange rates.
     </p>
   );
 
