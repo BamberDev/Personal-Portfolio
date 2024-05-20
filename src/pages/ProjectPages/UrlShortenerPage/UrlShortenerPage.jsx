ad59f0cbc0960fc73843751efc824e61ccd289dc
Bamberek <kabaczek99@gmail.com>
2024-05-20 23:54:17 +0200

    updated project description

--- src/pages/ProjectPages/UrlShortenerPage/UrlShortenerPage.jsx
+++ src/pages/ProjectPages/UrlShortenerPage/UrlShortenerPage.jsx
@@ -4,7 +4,14 @@ import projects from "../../../data/projectListOptions";
 const UrlShortenerPage = () => {
   const { src, alt, title, code, demo, techStack } = projects[3];
 
-  const content = <p>SOME TEXT</p>;
+  const content = (
+    <p>
+      URL Shortener is a web application that allows users to shorten long URLs
+      into compact, easy-to-share links. This application is designed to enhance
+      the user experience by providing a simple and efficient way to manage
+      URLs.
+    </p>
+  );
 
   return (
     <ProjectPage
